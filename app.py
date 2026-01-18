0101d5655b9cd3bb1c361d5599bdfd34ba9aaad5
alisapuzo <alisapuzo315@gmail.com>
2026-01-18 19:57:52 +0100

    Merge branch 'main' into feature

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/list/')
+def lists():
+    return "TODO: This is the list page"
+
+@app.route('/list/<int:id>')
+def list(id):
+    return f"TODO: This is the list page for list with id {id}"
+
